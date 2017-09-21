f296618686d6a8d45baef1f5d2eb5ca50ab55797
Jamie Jones <jbjonesjr@github.com>
2017-09-21 11:04:08 -0400

    make the server port an environment varibale option

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || process.env.EXPRESS_SERVER_PORT || 3000;
+
 app.get('/', function (req, res) {
   res.send('Welcome to this basic app!')
 })
 
-app.listen(3000, function () {
-  console.log('Express app initiated and listening on port 3000!')
+app.listen(server_port, function () {
+  console.log('Express app initiated and listening on port '+server_port+'!')
 })
