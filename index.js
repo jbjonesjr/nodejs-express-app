9b0089c5700060ac82956edb5855312ea97ed16c
Jamie Jones <jbjonesjr@github.com>
2017-09-21 22:36:49 -0400

    start creating html framework

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express()
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || process.env.EXPRESS_SERVER_PORT || 3000;
 
+var html_frame = "<html>\
+  <head>\
+  </head>\
+  <body>\
+  </body>\
+</html>";
 app.get('/', function (req, res) {
   res.send('Welcome to this basic app! \
   You can now find this available on <a href="http://express-main.apps.ocp.test-demo-dlt.com/">OpenShift</a>');
